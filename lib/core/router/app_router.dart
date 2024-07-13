a270b67cc54ed00d3773094816b22211dc1a7365
Ramez Nasser <rameznasser55@gmail.com>
2024-07-13 10:25:36 +0300

    Refactor main app and add routing with go_router
    - Renamed MyApp to ECommerceApp for better clarity.
    - Introduced go_router for routing and updated main.dart to use it.
    - Added app_router.dart to manage application routes.
    - Updated pubspec.yaml and pubspec.lock to include go_router dependency.
    - Modified widget_test.dart to reflect the new app name.

---
+++ lib/core/router/app_router.dart
@@ -0,0 +1,14 @@
+import 'package:go_router/go_router.dart';
+
+abstract class AppRouter {
+  static const initialRoute = '/';
+
+  static final GoRouter routes = GoRouter(
+    routes: [
+      GoRoute(
+        path: initialRoute,
+        builder: (context, state) => const SplachView(),
+      ),
+    ],
+  );
+}
