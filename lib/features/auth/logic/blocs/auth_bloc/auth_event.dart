a6bcac7a2971fef6dd37d94e8595614bd6dcb4ba
Ramez Nasser <rameznasser55@gmail.com>
2024-07-13 19:50:04 +0300

    - add loading, success and failure states for login & signup events

--- lib/features/auth/logic/blocs/auth_bloc/auth_event.dart
+++ lib/features/auth/logic/blocs/auth_bloc/auth_event.dart
@@ -2,3 +2,7 @@ part of 'auth_bloc.dart';
 
 @immutable
 sealed class AuthEvent {}
+
+final class AuthLoginEvent extends AuthEvent {}
+
+final class AuthSignupEvent extends AuthEvent {}
--- lib/features/auth/logic/blocs/auth_bloc/auth_state.dart
+++ lib/features/auth/logic/blocs/auth_bloc/auth_state.dart
@@ -4,3 +4,15 @@ part of 'auth_bloc.dart';
 sealed class AuthState {}
 
 final class AuthInitial extends AuthState {}
+
+final class AuthLoginLoading extends AuthState {}
+
+final class AuthLoginSuccess extends AuthState {}
+
+final class AuthLoginFailure extends AuthState {}
+
+final class AuthSignupLoading extends AuthState {}
+
+final class AuthSignupSuccess extends AuthState {}
+
+final class AuthSignupFailure extends AuthState {}
