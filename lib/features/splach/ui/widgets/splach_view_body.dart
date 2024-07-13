5ef30e94ccdef88f88503b2d7627124fda01efa5
Ramez Nasser <rameznasser55@gmail.com>
2024-07-13 11:29:00 +0300

    Add SVG assets and update Splash screen
    - Added SVG assets "just do it..svg" and "you have the power to.svg" to the assets/images directory.
    - Updated `splach_view_body.dart` to display the new SVG images using `flutter_svg` package.
    - Disabled debug banner in `main.dart`.
    - Updated `pubspec.yaml` to include `flutter_svg` dependency.
    - Updated `pubspec.lock` to reflect new dependencies.

--- lib/features/splach/ui/widgets/splach_view_body.dart
+++ lib/features/splach/ui/widgets/splach_view_body.dart
@@ -1,10 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class SplachViewBody extends StatelessWidget {
   const SplachViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: const Color(0xff1483C2),
+      body: Column(children: [
+        SvgPicture.asset(
+          'assets/images/you have the power to.svg',
+        ),
+        SvgPicture.asset(
+          'assets/images/just do it..svg',
+        ),
+      ]),
+    );
   }
 }
