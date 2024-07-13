fe26dc86843b1eef59b31fabe4917a1b5be2eb33
Ramez Nasser <rameznasser55@gmail.com>
2024-07-13 14:09:37 +0300

    - add home router for navigation - add login view and login view body - refactor splach view body

--- lib/core/router/app_router.dart
+++ lib/core/router/app_router.dart
@@ -1,14 +1,20 @@
+import 'package:e_commerce/features/auth/ui/views/login_view.dart';
 import 'package:e_commerce/features/splach/ui/views/splach_view.dart';
 import 'package:go_router/go_router.dart';
 
 abstract class AppRouter {
   static const initialRoute = '/';
+  static const loginRoute = '/loginview';
 
   static final GoRouter routes = GoRouter(
     routes: [
       GoRoute(
         path: initialRoute,
         builder: (context, state) => const SplachView(),
+      ),
+      GoRoute(
+        path: loginRoute,
+        builder: (context, state) => const LoginView(),
       ),
     ],
   );
--- lib/features/splach/ui/widgets/splach_view_body.dart
+++ lib/features/splach/ui/widgets/splach_view_body.dart
@@ -1,21 +1,56 @@
+import 'package:e_commerce/core/router/app_router.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:go_router/go_router.dart';
 
-class SplachViewBody extends StatelessWidget {
+class SplachViewBody extends StatefulWidget {
   const SplachViewBody({super.key});
+
+  @override
+  State<SplachViewBody> createState() => _SplachViewBodyState();
+}
+
+class _SplachViewBodyState extends State<SplachViewBody> {
+  @override
+  void initState() {
+    Future.delayed(
+      const Duration(seconds: 3),
+      () {
+        GoRouter.of(context).go(AppRouter.loginRoute);
+      },
+    );
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color(0xff1483C2),
-      body: Column(children: [
-        SvgPicture.asset(
-          'assets/images/you have the power to.svg',
+      body: Padding(
+        padding: EdgeInsets.all(
+          MediaQuery.sizeOf(context).width * 0.05,
         ),
-        SvgPicture.asset(
-          'assets/images/just do it..svg',
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SvgPicture.asset(
+              'assets/images/you have the power to.svg',
+            ),
+            const SizedBox(
+              height: 36,
+            ),
+            SvgPicture.asset(
+              'assets/images/just do it..svg',
+            ),
+            const SizedBox(
+              height: 36,
+            ),
+            SvgPicture.asset(
+              'assets/images/image 3.svg',
+            ),
+          ],
         ),
-      ]),
+      ),
     );
   }
 }
